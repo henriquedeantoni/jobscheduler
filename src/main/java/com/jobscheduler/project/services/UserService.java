1b8bdba804d353fe0b097a2e7812eb6a8471fdbe
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-08-17 10:40:28 -0300

    feat: implemented service dependence injection in resource and user controller class, introduced new metohd on user service class with findbyid method

--- src/main/java/com/jobscheduler/project/services/UserService.java
+++ src/main/java/com/jobscheduler/project/services/UserService.java
@@ -1,18 +1,27 @@
 package com.jobscheduler.project.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.jobscheduler.project.entities.User;
 import com.jobscheduler.project.repositories.UserRepository;
 
-public class UserServices {
+
+@Service
+public class UserService {
 
 	@Autowired
 	private UserRepository repository;
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+	 	Optional<User> obj =  repository.findById(null);
+		return obj.get();
 	}
 }
