59f81dbf706202a93673d3aff2ef615883c28233
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-08-19 21:44:15 -0300

    feat: created job entity class

---
+++ src/main/java/com/jobscheduler/project/entities/Job.java
@@ -0,0 +1,97 @@
+package com.jobscheduler.project.entities;
+
+public class Job {
+	
+	private Long id;
+	private String title;
+	private String description;
+	private Integer duration;
+	private Double price;
+	private Double hourPrice;
+	
+	public Job() {
+	}
+
+	public Job(Long id, String title, String description, Integer duration, Double price, Double hourPrice) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.duration = duration;
+		this.price = price;
+		this.hourPrice = hourPrice;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Double getHourPrice() {
+		return hourPrice;
+	}
+
+	public void setHourPrice(Double hourPrice) {
+		this.hourPrice = hourPrice;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Job other = (Job) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
