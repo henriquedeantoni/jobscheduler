23ab5ff6ce212f81a13477d72d2a7f9f809bed0a
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-08-21 23:35:13 -0300

    feat: update class job with manytomany relation to category

--- src/main/java/com/jobscheduler/project/entities/Job.java
+++ src/main/java/com/jobscheduler/project/entities/Job.java
@@ -8,6 +8,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,6 +28,11 @@ public class Job implements Serializable {
 	private Double price;
 	private Double hourPrice;
 	
+	@ManyToMany
+	@JoinTable(name = "tb_job_category", 
+	joinColumns = @JoinColumn(name = "job_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id")
+			)
 	private Set<Category> categories = new HashSet<>();
 	
 	public Job() {
