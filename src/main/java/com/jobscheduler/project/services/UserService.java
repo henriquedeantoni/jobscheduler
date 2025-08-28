90ca67ea30dd6f118963002d770623374d40e737
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-08-28 13:31:09 -0300

    fix: instant timestamp format correction

--- src/main/java/com/jobscheduler/project/services/UserService.java
+++ src/main/java/com/jobscheduler/project/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.jobscheduler.project.entities.User;
 import com.jobscheduler.project.repositories.UserRepository;
+import com.jobscheduler.project.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 	 	Optional<User> obj =  repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
