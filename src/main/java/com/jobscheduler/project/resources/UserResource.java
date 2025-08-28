09ef7d937687a30b85b9db61980533f7bdb8419f
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-08-27 21:50:36 -0300

    feat: updated new methods user services

--- src/main/java/com/jobscheduler/project/resources/UserResource.java
+++ src/main/java/com/jobscheduler/project/resources/UserResource.java
@@ -1,15 +1,18 @@
 package com.jobscheduler.project.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jobscheduler.project.entities.User;
 import com.jobscheduler.project.services.UserService;
@@ -37,6 +40,13 @@ public class UserResource {
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User user){
 		user = service.insert(user);
-		return ResponseEntity.ok().body(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<User> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
