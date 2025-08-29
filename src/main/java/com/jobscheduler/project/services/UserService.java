fdac944848186d867b0ef8b9bfcd89343922e6e7
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-08-28 21:52:10 -0300

    feat: added try catch and data exception on delete method

--- src/main/java/com/jobscheduler/project/services/UserService.java
+++ src/main/java/com/jobscheduler/project/services/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.jobscheduler.project.entities.User;
@@ -31,8 +33,26 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);	
+		} catch (EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			e.printStackTrace();
+		}
+	}	
+	
+	/*
+	 * 
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);	
+		} catch (RuntimeException e){
+			e.printStackTrace();
+		}
 	}
+	 * 
+	 */
 	
 	public User update(Long id, User user) {
 		User entity = repository.getReferenceById(id);
