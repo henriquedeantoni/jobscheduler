c383ac5653794da8796d76e2a2cea72521dd9569
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-09-01 23:46:57 -0300

    feat: new resource on category class

--- src/main/java/com/jobscheduler/project/resources/CategoryResource.java
+++ src/main/java/com/jobscheduler/project/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.jobscheduler.project.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jobscheduler.project.entities.Category;
 import com.jobscheduler.project.services.CategoryService;
@@ -29,5 +33,12 @@ public class CategoryResource {
 	public ResponseEntity<Category> findById(@PathVariable Long id){
 		Category category = categoryService.findById(id);
 		return ResponseEntity.ok().body(category);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> insert(@RequestBody Category category){
+		category = categoryService.insert(category);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(category.getId()).toUri();
+		return ResponseEntity.created(uri).body(category);
 	}
 }
