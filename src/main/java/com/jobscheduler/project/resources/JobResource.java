5fded28fda7fd23810c7cfbc7416b7883df8dc92
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-09-02 23:07:54 -0300

    feat: added new endpoint for find by category rule

--- src/main/java/com/jobscheduler/project/resources/JobResource.java
+++ src/main/java/com/jobscheduler/project/resources/JobResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jobscheduler.project.entities.Category;
 import com.jobscheduler.project.entities.Job;
 import com.jobscheduler.project.services.JobService;
 
@@ -29,5 +30,11 @@ public class JobResource {
 	public ResponseEntity<Job> findById(@PathVariable Long id){
 		Job job = jobService.findById(id);
 		return ResponseEntity.ok().body(job);
+	}
+	
+	@GetMapping(value = "/{category}")
+	public ResponseEntity<List<Job>> findByCategory(@PathVariable Category category){
+		List<Job> list = jobService.findByCategory(category);
+		return ResponseEntity.ok().body(list);
 	}
 }
