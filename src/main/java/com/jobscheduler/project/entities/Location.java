acef0fe345a7390b57cbd0b8be7c1cff2264801e
Henrique de Antoni <henriqdeantoni@gmail.com>
2025-09-11 21:14:56 -0300

    feat: entity location

---
+++ src/main/java/com/jobscheduler/project/entities/Location.java
@@ -0,0 +1,97 @@
+package com.jobscheduler.project.entities;
+
+public class Location {
+
+	private String id;
+	private String street;
+	private String number;
+	private String county;
+	private String city;
+	private String postalCode;
+	
+	public Location() {
+	}
+
+	public Location(String id, String street, String number, String county, String city, String postalCode) {
+		super();
+		this.id = id;
+		this.street = street;
+		this.number = number;
+		this.county = county;
+		this.city = city;
+		this.postalCode = postalCode;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getCounty() {
+		return county;
+	}
+
+	public void setCounty(String county) {
+		this.county = county;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+}
